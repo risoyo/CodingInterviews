1677da08a1c1502313c5982d7aa34b10db50d544
risoyo <yinglongnvba@gmail.com>
2017-08-08 22:11:16 +0800

    用栈实现队列

--- 剑指offer-牛客/05-用两个栈实现队列.py
+++ 剑指offer-牛客/05-用两个栈实现队列.py
@@ -1,20 +1,38 @@
-# -*- coding:utf-8 -*-
+#coding=utf-8
 from Functions import Stack
 
 
 class Solution:
     def __init__(self):
-        self.items = Stack.Stack()
+        self.items1 = Stack.Stack()
+        self.items2 = Stack.Stack()
 
     def push(self, node):
-        self.items.push(node)
+        self.items1.push(node)
 
     def pop(self):
-        self.items.pop()
+        if self.items2.isEmpty():
+            while self.items1.size() > 0:
+                self.items2.push(self.items1.pop())
+        return self.items2.pop()
+
+    def isEmpty(self):
+        if self.items1.size() > 0 or self.items2.size() > 0:
+            return 1
+        else:
+            return 0
 
 
 if __name__ == '__main__':
-    dic = Stack.Stack()
-    dic.push('111')
-    print dic.size()
+    dic = Solution()
+    dic.push('a')
+    dic.push('b')
     print dic.pop()
+    dic.push('c')
+    dic.push('d')
+    print dic.pop()
+    print dic.pop()
+    dic.push('e')
+    print dic.pop()
+    print dic.pop()
+
