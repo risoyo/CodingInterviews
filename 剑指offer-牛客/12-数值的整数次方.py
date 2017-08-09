19defca1c5df17245aa8643434a3a490c7835e2f
risoyo <yinglongnvba@gmail.com>
2017-08-09 21:47:24 +0800

    null

--- 剑指offer-牛客/12-数值的整数次方.py
+++ 剑指offer-牛客/12-数值的整数次方.py
@@ -9,7 +9,7 @@ class Solution:
         for i in range(1, abs(exponent)):  # 乘exponent次
             base *= number
         if exponent < 0:  # 若幂小于0,则返回乘积分之一
-            return 1 / float(base)
+            return 1 / float(base)  # 在计算除法时,分子分母中的一个为float型则结果为float型,否则为int型,不加float(base)的话除得的结果为0
         return base
 
 
