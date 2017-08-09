9943e9d537e1745952a0727468028d0b34c58878
risoyo <yinglongnvba@gmail.com>
2017-08-10 00:57:28 +0800

    更新注释

--- Functions/ListNode.py
+++ Functions/ListNode.py
@@ -23,17 +23,17 @@ class LinkedList:
             self.tail = node
 
     def delete_node(self, link_list, list_delete):
-        if link_list.head.next is None and link_list.head.data == list_delete:
+        if link_list.head.next is None and link_list.head.data == list_delete:  # 当需要删除的为头节点且链表中只有一个节点时,直接将其置为None
             link_list.head = None
-        elif link_list.tail.data == list_delete:
+        elif link_list.tail.data == list_delete:  # 当需要删除的节点为尾节点时,遍历链表,找到链表倒数第二个节点,然后使其next为next.next
             while link_list.head.next is not link_list.tail:
                 link_list.head = link_list.head.next
             link_list.head.next = None
-        else:
+        else:  # 正常删除,只对链表遍历一遍,时间复杂度趋近于o(1)
             while link_list.head.next is not None:
-                if link_list.head.data == list_delete:
-                    link_list.head.data = link_list.head.next.data
-                    link_list.head.next = link_list.head.next.next
+                if link_list.head.data == list_delete:  # 当找到需要删除的节点时
+                    link_list.head.data = link_list.head.next.data  # 将下一个节点的值赋给当前节点
+                    link_list.head.next = link_list.head.next.next  # 然后使其next为next.next,删除下一个节点
                     break
                 link_list.head = link_list.head.next
 
