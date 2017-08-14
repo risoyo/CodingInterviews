7f1e2357383a18dd32661e5fa5a1a12de675c0e9
risoyo <yinglongnvba@gmail.com>
2017-08-14 12:02:19 +0800

    用三个指针逆转整个链表,终于会写牛客的链表了=_=

--- 剑指offer-牛客/15-反转链表.py
+++ 剑指offer-牛客/15-反转链表.py
@@ -3,6 +3,8 @@ class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
+
+
 class Solution:
     # 返回ListNode
     def ReverseList(self, pHead):
@@ -24,11 +26,11 @@ if __name__ == '__main__':
     num = [1, 2, 3, 4, 5]
     link_list = ListNode(num[0])
     link_head = link_list
-    for i in range(1,len(num)):
+    for i in range(1, len(num)):
         node = ListNode(num[i])
         link_head.next = node
         link_head = link_head.next
     link_reverse = bat.ReverseList(link_list)
     while link_reverse is not None:
-        print link_reverse.val
+        print link_reverse.val,
         link_reverse = link_reverse.next
