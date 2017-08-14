539ac9b3df1d25f9b0dd87bcb42bb382738151b7
risoyo <yinglongnvba@gmail.com>
2017-08-14 22:13:48 +0800

    加入前中后序三种遍历方法

--- 剑指offer-牛客/04-重建二叉树.py
+++ 剑指offer-牛客/04-重建二叉树.py
@@ -22,6 +22,24 @@ class Solution:
             root.right = self.ConstructBinaryTreeCore(rpre, rtin)
         return root  # 返回构建好的节点
 
+    def pre_order(self, tree):  # 前序遍历二叉树
+        if tree is not None:
+            print tree.val,
+            self.pre_order(tree.left)
+            self.pre_order(tree.right)
+
+    def min_order(self, tree):  # 中序遍历二叉树
+        if tree is not None:
+            self.min_order(tree.left)
+            print tree.val,
+            self.min_order(tree.right)
+
+    def post_order(self, tree):  # 后序遍历二叉树
+        if tree is not None:
+            self.post_order(tree.left)
+            self.post_order(tree.right)
+            print tree.val,
+
     def reConstructBinaryTree(self, pre, tin):
         if pre == 0 or tin == 0:
             return False
@@ -33,12 +51,13 @@ if __name__ == '__main__':
     pre = [1, 2, 4, 7, 3, 5, 6, 8]
     tin = [4, 7, 2, 1, 5, 3, 8, 6]
     tree = bat.reConstructBinaryTree(pre, tin)
-    treelist = [tree]  # 按层次遍历二叉树,先将头结点入队
-    while len(treelist) > 0:  # 队列非空时遍历就没有完成
-        print treelist[0].val,  # 输出队列最前端节点的值,加个','逗号后在console中输出的值就不会每个都跟着回车了
-        if treelist[0].left is not None:  # 如果该节点有左子树,则将其加入队列
-            treelist.append(treelist[0].left)
-        if treelist[0].right is not None:  # 如果该节点有右子树,则将其加入队列
-            treelist.append(treelist[0].right)
-        del treelist[0]  # 将队列最前端的节点删除
+    bat.min_order(tree)
+    # treelist = [tree]  # 按层次遍历二叉树,先将头结点入队
+    # while len(treelist) > 0:  # 队列非空时遍历就没有完成
+    #     print treelist[0].val,  # 输出队列最前端节点的值,加个','逗号后在console中输出的值就不会每个都跟着回车了
+    #     if treelist[0].left is not None:  # 如果该节点有左子树,则将其加入队列
+    #         treelist.append(treelist[0].left)
+    #     if treelist[0].right is not None:  # 如果该节点有右子树,则将其加入队列
+    #         treelist.append(treelist[0].right)
+    #     del treelist[0]  # 将队列最前端的节点删除
 
