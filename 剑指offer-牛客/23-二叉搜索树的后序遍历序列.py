73dd8f4d40373231b643e2c3fad93944ed32070c
risoyo <yinglongnvba@gmail.com>
2017-08-20 12:08:48 +0800

    用最后一个值分割序列即可

---
+++ 剑指offer-牛客/23-二叉搜索树的后序遍历序列.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+class Solution:
+    def VerifySquenceOfBST(self, sequence):
+        right = None
+        if len(sequence) == 0:
+            return False
+        for i in range(len(sequence)):
+            if sequence[i] > sequence[-1]:
+                right = i
+                break
+        if right is None:
+            return True
+        for i in range(right, len(sequence)-1):
+            if sequence[i] < sequence[-1]:
+                return False
+        return True
+
+
+if __name__ == '__main__':
+    bat = Solution()
+    tree1 = [5, 7, 6, 9, 11, 10, 8]
+    tree2 = [7, 4, 6, 5]
+    tree3 = []
+    tree4 = [1, 2, 3, 6]
+    print bat.VerifySquenceOfBST(tree4)
