937c22c7fe11734f91e2ca47f378aa5af7b43b56
risoyo <yinglongnvba@gmail.com>
2017-08-31 17:12:50 +0800

    丑数都由2,3,5乘得,保存每个丑数

---
+++ 剑指offer-牛客/33-丑数.py
@@ -0,0 +1,24 @@
+# -*- coding:utf-8 -*-
+class Solution:
+    def GetUglyNumber_Solution(self, index):
+        if index <= 0:
+            return 0
+        uglyList = [1]
+        indexTwo = 0
+        indexThree = 0
+        indexFive = 0
+        for i in range(index - 1):
+            newUgly = min(uglyList[indexTwo] * 2, uglyList[indexThree] * 3, uglyList[indexFive] * 5)
+            uglyList.append(newUgly)
+            if newUgly % 2 == 0:
+                indexTwo += 1
+            if newUgly % 3 == 0:
+                indexThree += 1
+            if newUgly % 5 == 0:
+                indexFive += 1
+        return uglyList[-1]
+
+
+if __name__ == '__main__':
+    bat = Solution()
+    print bat.GetUglyNumber_Solution(1500)
